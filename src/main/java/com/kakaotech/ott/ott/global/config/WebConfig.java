c19a3c7b8a5a1178937c3eb8494a8edcc73a7f6e
Parkyuchan <dbcks4848@naver.com>
2025-05-08 15:24:45 +0900

    feat: config 변경

--- src/main/java/com/kakaotech/ott/ott/global/config/WebConfig.java
+++ src/main/java/com/kakaotech/ott/ott/global/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("https://*.onthe-top.com", "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("Authorization", "Content-Type")
                         .allowCredentials(true); // 쿠키 허용 시 true
             }
         };
