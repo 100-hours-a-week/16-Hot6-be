f8ea907fe28a37a3c9cfc661f62355471850747a
Parkyuchan <dbcks4848@naver.com>
2025-05-09 18:23:18 +0900

    feat: AI 게시글 생성 시 해당 AI 이미지에 postId 값 적용

--- src/main/java/com/kakaotech/ott/ott/aiImage/domain/repository/AiImageRepository.java
+++ src/main/java/com/kakaotech/ott/ott/aiImage/domain/repository/AiImageRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 
 public interface AiImageRepository {
 
+    AiImage savePost(AiImage aiImage);
+
     AiImage save(AiImage aiImage);
 
     Optional<AiImageEntity> findById(Long userId);
