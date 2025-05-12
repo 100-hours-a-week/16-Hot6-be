eb097b3a22b77f8584cf1737789672b806347cd5
Parkyuchan <dbcks4848@naver.com>
2025-05-12 21:53:44 +0900

    fix: 자유 게시판 작성 수정

--- src/main/java/com/kakaotech/ott/ott/post/presentation/dto/request/FreePostCreateRequestDto.java
+++ src/main/java/com/kakaotech/ott/ott/post/presentation/dto/request/FreePostCreateRequestDto.java
@@ -23,7 +23,6 @@ public class FreePostCreateRequestDto {
     @NotBlank(message = "게시글 내용은 필수입니다.")
     private String content;
 
-    @NotNull(message = "이미지는 필수입니다.")
     @Size(max = 5, message = "이미지는 최대 5개까지 업로드할 수 있습니다.")
     private List<MultipartFile> images;
 }
