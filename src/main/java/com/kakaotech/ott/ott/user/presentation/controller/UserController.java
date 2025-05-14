130ecefd9d5d35e86116d919fd27e70b5a3cbff3
Parkyuchan <dbcks4848@naver.com>
2025-05-15 05:18:56 +0900

    feat: 사용자 탈퇴 시 기본 프로필 설정

--- src/main/java/com/kakaotech/ott/ott/user/presentation/controller/UserController.java
+++ src/main/java/com/kakaotech/ott/ott/user/presentation/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/v1/users")
