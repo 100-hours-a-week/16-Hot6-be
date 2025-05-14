e8230083f54abb66c99972ce878f14fb269d5911
Parkyuchan <dbcks4848@naver.com>
2025-05-15 04:42:55 +0900

    fix: 쿼리 파라미터명 변경

--- src/main/java/com/kakaotech/ott/ott/user/presentation/controller/UserController.java
+++ src/main/java/com/kakaotech/ott/ott/user/presentation/controller/UserController.java
@@ -38,18 +38,18 @@ public class UserController {
     @GetMapping("/me/desks")
     public ResponseEntity<ApiResponse<MyDeskImageResponseDto>> getMyDesk(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
-            @RequestParam (value = "lastId", required = false) Long lastId,
+            @RequestParam (value = "cursorId", required = false) Long cursorId,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(value = "type", required = false) String type) {
 
         Long userId = userPrincipal.getId();
 
         // lastId가 없으면 기본값 (가장 최신 ID) 설정
-        if (lastId == null) {
-            lastId = Long.MAX_VALUE; // 가장 최신 ID를 의미
+        if (cursorId == null) {
+            cursorId = Long.MAX_VALUE; // 가장 최신 ID를 의미
         }
 
-        MyDeskImageResponseDto myDeskImageResponseDto = userService.getMyDeskWithCursor(userId, lastId, size, type);
+        MyDeskImageResponseDto myDeskImageResponseDto = userService.getMyDeskWithCursor(userId, cursorId, size, type);
 
         return ResponseEntity.ok(ApiResponse.success("나의 데스크 조회 성공", myDeskImageResponseDto));
     }
