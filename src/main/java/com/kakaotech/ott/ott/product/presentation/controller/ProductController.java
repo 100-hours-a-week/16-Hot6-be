d58455f1aad986f932ff64539c1f982ced237cd8
junsik <deeptree97@gmail.com>
2025-06-11 16:37:47 +0900

    feat: 상품 상세 조회 엔드포인트

--- src/main/java/com/kakaotech/ott/ott/product/presentation/controller/ProductController.java
+++ src/main/java/com/kakaotech/ott/ott/product/presentation/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.kakaotech.ott.ott.product.application.service.ProductService;
 import com.kakaotech.ott.ott.product.presentation.dto.request.VariantDto;
+import com.kakaotech.ott.ott.product.presentation.dto.response.ProductGetResponseDto;
 import com.kakaotech.ott.ott.user.domain.model.UserPrincipal;
 import jakarta.validation.constraints.Size;
 import org.springframework.http.HttpStatus;
@@ -12,10 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kakaotech.ott.ott.global.response.ApiResponse;
@@ -52,5 +50,16 @@ public class ProductController {
         
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success("상품 등록 완료", productCreateResponseDto));
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ApiResponse<ProductGetResponseDto>> getProductDetail(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @PathVariable Long productId) {
+        Long userId = (userPrincipal == null) ? null : userPrincipal.getId();
+
+        ProductGetResponseDto productDetail = productService.getProduct(productId, userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("상품 상세 정보 조회 성공", productDetail));
     }
 }
