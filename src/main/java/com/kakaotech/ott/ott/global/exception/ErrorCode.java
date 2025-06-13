10a453a5835b9caec5499053b58bf4eb96ad1a39
junsik <deeptree97@gmail.com>
2025-06-13 22:41:42 +0900

    feat: ACTIVE 상태가 아닌 품목에 대한 예약불가 에러

--- src/main/java/com/kakaotech/ott/ott/global/exception/ErrorCode.java
+++ src/main/java/com/kakaotech/ott/ott/global/exception/ErrorCode.java
@@ -77,6 +77,7 @@ public enum ErrorCode {
     INVALID_PRODUCT_STATUS(HttpStatus.BAD_REQUEST, "유효하지 않은 상품 상태입니다."),
     INVALID_DISCOUNT(HttpStatus.BAD_REQUEST, "할인가는 정가보다 클 수 없습니다."),
     FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
+    INVALID_VARIANT_STATUS(HttpStatus.BAD_REQUEST, "품목 상태가 올바르지 않습니다."),
 
     // ORDERITEM
     NOT_ORDERED_ITEM_STATE(HttpStatus.BAD_REQUEST, "주문 대기 상태가 아닙니다."),
