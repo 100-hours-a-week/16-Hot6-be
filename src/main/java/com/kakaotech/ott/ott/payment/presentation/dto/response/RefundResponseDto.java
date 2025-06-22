f945049ce9d6460d7cf033fb7782f208260f072e
junsik <deeptree97@gmail.com>
2025-06-22 23:16:03 +0900

    feat: 환불 요청/응답 DTO

---
+++ src/main/java/com/kakaotech/ott/ott/payment/presentation/dto/response/RefundResponseDto.java
@@ -0,0 +1,17 @@
+package com.kakaotech.ott.ott.payment.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefundResponseDto {
+    private Long orderId;
+}
