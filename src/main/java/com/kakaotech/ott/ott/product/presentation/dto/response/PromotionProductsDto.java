3a4233b4f2cdff1a908ab00ec5b1574e172a13c7
Parkyuchan <dbcks4848@naver.com>
2025-06-26 15:41:06 +0900

    feat: dto 변경

--- src/main/java/com/kakaotech/ott/ott/product/presentation/dto/response/PromotionProductsDto.java
+++ src/main/java/com/kakaotech/ott/ott/product/presentation/dto/response/PromotionProductsDto.java
@@ -1,4 +1,26 @@
 package com.kakaotech.ott.ott.product.presentation.dto.response;
 
+import com.kakaotech.ott.ott.product.domain.model.PromotionType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PromotionProductsDto {
+
+    private Long productId;
+
+    private String productName;
+
+    private String imageUuid;
+
+    private int price;
+
+    private PromotionType promotionType;
+
+    private boolean scrapped;
 }
