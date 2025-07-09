f4bc198fbb8e8a3d352a6f00207f9d8c34405031
Parkyuchan <dbcks4848@naver.com>
2025-07-09 16:58:34 +0900

    feat: 주석 제거

--- src/main/java/com/kakaotech/ott/ott/payment/application/serviceImpl/PaymentServiceImpl.java
+++ src/main/java/com/kakaotech/ott/ott/payment/application/serviceImpl/PaymentServiceImpl.java
@@ -1,9 +1,6 @@
 package com.kakaotech.ott.ott.payment.application.serviceImpl;
 
-import com.kakaotech.ott.ott.global.exception.CustomException;
-import com.kakaotech.ott.ott.global.exception.ErrorCode;
 import com.kakaotech.ott.ott.orderItem.domain.model.OrderItem;
-import com.kakaotech.ott.ott.orderItem.domain.model.OrderItemStatus;
 import com.kakaotech.ott.ott.orderItem.domain.model.RefundReason;
 import com.kakaotech.ott.ott.orderItem.domain.repository.OrderItemRepository;
 import com.kakaotech.ott.ott.payment.application.event.PaymentCompletedEvent;
@@ -16,17 +13,9 @@ import com.kakaotech.ott.ott.payment.presentation.dto.request.PaymentRequestDto;
 import com.kakaotech.ott.ott.payment.presentation.dto.request.RefundRequestDto;
 import com.kakaotech.ott.ott.payment.presentation.dto.response.PaymentResponseDto;
 import com.kakaotech.ott.ott.payment.presentation.dto.response.RefundResponseDto;
-import com.kakaotech.ott.ott.pointHistory.domain.model.PointActionReason;
-import com.kakaotech.ott.ott.pointHistory.domain.model.PointActionType;
 import com.kakaotech.ott.ott.pointHistory.domain.model.PointHistory;
 import com.kakaotech.ott.ott.pointHistory.domain.repository.PointHistoryRepository;
-import com.kakaotech.ott.ott.product.domain.model.Product;
-import com.kakaotech.ott.ott.product.domain.model.ProductPromotion;
-import com.kakaotech.ott.ott.product.domain.model.ProductVariant;
-import com.kakaotech.ott.ott.product.domain.repository.ProductPromotionRepository;
-import com.kakaotech.ott.ott.product.domain.repository.ProductVariantRepository;
 import com.kakaotech.ott.ott.productOrder.domain.model.ProductOrder;
-import com.kakaotech.ott.ott.productOrder.domain.model.ProductOrderStatus;
 import com.kakaotech.ott.ott.productOrder.domain.repository.ProductOrderRepository;
 import com.kakaotech.ott.ott.user.domain.model.User;
 import com.kakaotech.ott.ott.user.domain.repository.UserRepository;
@@ -49,69 +38,7 @@ public class PaymentServiceImpl implements PaymentService {
     private final ProductOrderRepository productOrderRepository;
     private final PointHistoryRepository pointHistoryRepository;
     private final OrderItemRepository orderItemRepository;
-    private final ProductPromotionRepository productPromotionRepository;
-    private final ProductVariantRepository productVariantRepository;
     private final PaymentEventHandler paymentEventHandler;
-
-//    @Override
-//    @Transactional
-//    public PaymentResponseDto createPayment(PaymentRequestDto paymentRequestDto, Long userId, Long orderId) {
-//
-//        User user = userRepository.findById(userId);
-//
-//        PointHistory pointHistory = pointHistoryRepository.findLatestPointHistoryByUserId(userId);
-//
-//        if (pointHistory.getBalanceAfter() < paymentRequestDto.getPoint()) {
-//            throw new CustomException(ErrorCode.INSUFFICIENT_POINT_BALANCE);
-//        }
-//
-//        ProductOrder productOrder = productOrderRepository.findByIdAndUserIdToPayment(orderId, userId);
-//
-//        if (paymentRequestDto.getPoint() < productOrder.getSubtotalAmount() - productOrder.getDiscountAmount()) {
-//            throw new CustomException(ErrorCode.PAYMENT_POINT_BALANCE);
-//        }
-//
-//        int afterPaymentPoint = pointHistory.getBalanceAfter() - paymentRequestDto.getPoint();
-//
-//        PointHistory afterPointHistory = PointHistory.createPointHistory(userId, paymentRequestDto.getPoint(),
-//                afterPaymentPoint, PointActionType.DEDUCT, PointActionReason.PRODUCT_PURCHASE);
-//
-//        if (productOrder.getStatus().equals(ProductOrderStatus.PAID)) {
-//
-//            throw new CustomException(ErrorCode.ALREADY_PAID);
-//        }
-//
-//        productOrder.pay();
-//        productOrderRepository.paymentOrder(productOrder);
-//
-//        Payment payment = Payment.createPayment(orderId, PaymentMethod.POINT, paymentRequestDto.getPoint());
-//
-//        Payment savedPayment = paymentRepository.save(payment, user);
-//
-//        PointHistory savedPointHistory = pointHistoryRepository.save(afterPointHistory, user);
-//
-//        List<OrderItem> orderItemList = orderItemRepository.findByProductOrderId(productOrder.getId());
-//
-//        for(OrderItem orderItem : orderItemList) {
-//            if (orderItem.getPromotionId() != null) {
-//                ProductPromotion productPromotion = productPromotionRepository.findById(orderItem.getPromotionId());
-//                productPromotion.confirmPromotionSale(orderItem.getQuantity());
-//                productPromotionRepository.update(productPromotion);
-//
-//            } else {
-//                ProductVariant productVariant = productVariantRepository.findById(orderItem.getVariantsId());
-//                productVariant.confirmSale(orderItem.getQuantity());
-//                productVariantRepository.update(productVariant);
-//            }
-//
-//            if (orderItem.getStatus().equals(OrderItemStatus.PENDING))
-//                orderItem.pay();
-//        }
-//
-//        orderItemRepository.payOrderItem(orderItemList);
-//
-//        return new PaymentResponseDto(savedPointHistory.getId(), productOrder.getId(), savedPayment.getPaymentAmount(), new KstDateTime(savedPayment.getPaidAt()));
-//    }
 
     @Override
     @Transactional
