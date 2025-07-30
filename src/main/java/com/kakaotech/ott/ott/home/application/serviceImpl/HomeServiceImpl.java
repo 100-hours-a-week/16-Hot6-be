453bfa09b07e329aec55e86681f9d379ebf6aca8
junsik <deeptree97@gmail.com>
2025-07-30 11:40:20 +0900

    fix: 메인 페이지 캐시 하나로 통합

--- src/main/java/com/kakaotech/ott/ott/home/application/serviceImpl/HomeServiceImpl.java
+++ src/main/java/com/kakaotech/ott/ott/home/application/serviceImpl/HomeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kakaotech.ott.ott.home.application.serviceImpl;
 
+import com.kakaotech.ott.ott.global.cache.DistributedLock;
+import com.kakaotech.ott.ott.global.config.RedisConfig;
 import com.kakaotech.ott.ott.home.application.service.HomeService;
 import com.kakaotech.ott.ott.home.presentation.dto.response.MainResponseDto;
 import com.kakaotech.ott.ott.post.application.service.PostService;
@@ -8,6 +10,7 @@ import com.kakaotech.ott.ott.recommendProduct.application.service.ProductDomainS
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +20,15 @@ public class HomeServiceImpl implements HomeService {
     private final ProductService productService;
 
     // 로그인/비로그인 상태에 따라 다른 캐시
+    @Override
+    @Transactional(readOnly = true)
+    @Cacheable(cacheNames = RedisConfig.MAIN_CACHE, key = "#userId == null ? 'GUEST' : #userId")
     public MainResponseDto getMainPageData(Long userId) {
+        return getMainPageDataWithLock(userId);
+    }
+
+    @DistributedLock(keyPrefix = "main_page", key = "#userId")
+    private MainResponseDto getMainPageDataWithLock(Long userId) {
         return new MainResponseDto(
                 postService.getPopularSetups(userId),
                 productDomainService.getRecommendItems(userId),
